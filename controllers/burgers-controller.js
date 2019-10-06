b6211a05746271fdecc5b524b17e0b83f63b4b76
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-06 19:57:24 -0400

    Update burgers-controller.js

---
+++ controllers/burgers-controller.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var models = require("../models/burger.js");
+
+var app = express();
+
+var router = {
+  app.get("/", function(req, res) {
+  });
+  app.post
+};
+
+module.exports = router;
