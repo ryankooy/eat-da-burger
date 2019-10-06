1c0136a56290ab2cc80b71e5027bb622f0f9583a
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-06 19:18:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,3 +2,13 @@ var express = require("express");
 
 var app = express();
 var PORT = process.env.PORT || 3030;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./")(app);
+require("./")(app);
+
+app.listen(PORT, function() {
+  console.log(`App is listening on ${PORT} . . .`);
+});
