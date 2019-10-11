8d03cf1c3283bee1994345c3251bb7e36194cf98
Ryan Kooy <rwkooy@gmail.com>
2019-10-11 11:34:12 -0400

    server and heroku tests

--- config/connection.js
+++ config/connection.js
@@ -1,35 +1,23 @@
-// var mysql = require('mysql');
+require("dotenv").config();
+var mysql = require('mysql');
 
-// var conn = mysql.createConnection({
-//   host: 'localhost',
-//   port: 3306,
-//   user: 'root',
-//   password: '1289',
-//   database: 'burgers_db',
-//   details: {
-//     dialect: 'mysql'
-//   }
-// });
-
-// conn.connect(function(err) {
-//   if(err) {
-//     console.error(`Error connecting: ${err.stack}`);
-//     return;
-//   }
-//   console.log(`Connected as id ${conn.threadId}`);
-// });
-
-// module.exports = conn;
-
-module.exports = {
-  development: {
-    username: 'root',
-    password: '1289',
-    database: 'burgers_db',
-    details: {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql'
-    }
+var conn = mysql.createConnection({
+  host: 'g8mh6ge01lu2z3n1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  port: 3306,
+  user: 'yjtz1wsr8w7fhwlr',
+  password: process.env.JAWSDB_PASSWORD,
+  database: 'xnvmz8qdtdiya9em',
+  details: {
+    dialect: 'mysql'
   }
-};
+});
+
+conn.connect(function(err) {
+  if(err) {
+    console.error(`Error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`Connected as id ${conn.threadId}`);
+});
+
+module.exports = conn;
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,12 @@
+drop database if exists burgers_db;
+
+create database burgers_db;
+
 use burgers_db;
 
 create table burgers (
   id INT NOT NULL AUTO_INCREMENT,
-  burger_name VARCHAR(30) NOT NULL,
+  burger_name VARCHAR(50) NOT NULL,
   devoured BOOLEAN DEFAULT 0,
   primary key (id)
 );
