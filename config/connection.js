aff11face18eb5eb60bf51d4435e1a22087224c7
Ryan Kooy <rwkooy@gmail.com>
2019-10-10 23:59:27 -0400

    another heroku test

--- config/connection.js
+++ config/connection.js
@@ -31,22 +31,5 @@ module.exports = {
       port: 3306,
       dialect: 'mysql'
     }
-  },
-  test: {
-    username: process.env.TU,
-    password: process.env.TP || null,
-    database: 'project2_test',
-    details: {
-      host: 'localhost',
-      port: 3306,
-      dialect: 'mysql',
-      logging: false
-    }
-  },
-  production: {
-    'use_env_variable': 'JAWSDB_URL',
-    details: {
-      dialect: 'mysql'
-    }
   }
 };
