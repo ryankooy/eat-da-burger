2ffa4a76b6f27fd473392070f11d26b75c0e9a1e
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-13 20:58:02 -0400

    Update burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.selectAll(function(res) {
+    orm.selectAll("burgers", function(res) {
       cb(res);
     });
   },
   create: function(cols, vals, cb) {
-    orm.insertOne(cols, vals, function(res) {
+    orm.insertOne("burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.updateOne(objColVals, condition, function(res) {
+    orm.updateOne("burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
