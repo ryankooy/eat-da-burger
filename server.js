48cd5c10262d1343cb34fd4181bfea866b094f43
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-13 22:32:07 -0400

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.set("view engine", "handlebars");
 
 var routes = require("./controllers/burgers-controller.js");
 
-app.use("/", routes);
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log(`App is listening on ${PORT} . . .`);
