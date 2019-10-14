eac6e6a6fbdacdf8d3d0824691f1d12f2e5bc871
Ryan Kooy <rwkooy@gmail.com>
2019-10-14 00:31:30 -0400

    please

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,26 @@
 var conn = require("./connection.js");
 
-function questMarks(num) {
-  var arr = [];
-  for (var i = 0; i < num; i++) {
-    arr.push("?");
-  }
-  return arr.toString();
-}
+// function questMarks(num) {
+//   var arr = [];
+//   for (var i = 0; i < num; i++) {
+//     arr.push("?");
+//   }
+//   return arr.toString();
+// }
 
-function objectToSql(ob) {
-  var arr = [];
-  for (var key in ob) {
-    var value = ob[key];
-    if (Object.hasOwnProperty.call(ob, key)) {
-      if (typeof value === "string" && value.indexOf(" ") >= 0) {
-        value = `"${value}"`;
-      }
-      arr.push(`${key}=${value}`);
-    }
-  }
-  return arr.toString();
-}
+// function objectToSql(ob) {
+//   var arr = [];
+//   for (var key in ob) {
+//     var value = ob[key];
+//     if (Object.hasOwnProperty.call(ob, key)) {
+//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
+//         value = `"${value}"`;
+//       }
+//       arr.push(`${key}=${value}`);
+//     }
+//   }
+//   return arr.toString();
+// }
 
 var orm = {
   selectAll: function(input, cb) {
@@ -32,7 +32,7 @@ var orm = {
   },
   insertOne: function(table, cols, vals, cb) {
     var qS = "insert into " + table;
-    qS += ` (${cols.toString()}) values (${questMarks(vals.length)});`;
+    qS += ` (${cols}) values ("${vals}");`;
     console.log(qS);
 
     conn.query(qS, vals, function(err, res) {
@@ -42,7 +42,7 @@ var orm = {
   },
   updateOne: function(table, objColVals, condition, cb) {
     var qS = "update " + table;
-    qS += ` set ${objectToSql(objColVals)} where ${condition};`;
+    qS += ` set ${objColVals} where ${condition};`;
     console.log(qS);
 
     conn.query(qS, function(err, res) {
