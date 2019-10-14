c528454a1c2c0d6061a1ccfb4afd36bb150b48e3
Ryan Kooy <rwkooy@gmail.com>
2019-10-14 09:20:23 -0400

    index.handlebars devour button

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require("express");
 var methodOverride = require("method-override");
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 
 var app = express();
 var PORT = process.env.PORT || 3030;
 
 app.use(express.static(process.cwd() + "/public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 app.use(methodOverride("_method"));
 
