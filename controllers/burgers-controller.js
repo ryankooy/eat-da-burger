e37ec29ddecef1822260a5a905a6c3f402e13657
Ryan Kooy <48595828+ryankooy@users.noreply.github.com>
2019-10-13 22:31:35 -0400

    Update burgers-controller.js

--- controllers/burgers-controller.js
+++ controllers/burgers-controller.js
@@ -23,7 +23,7 @@ router.post("/", function(req, res) {
   });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/", function(req, res) {
   var condition = "id = " + req.params.id;
   burger.update({
      devoured: req.body.devoured
