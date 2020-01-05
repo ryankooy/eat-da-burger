82806855e1d7395394ec4523d12873ea673c9d84
Ryan Kooy <rwkooy@gmail.com>
2020-01-05 12:45:16 -0500

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,5 @@
 var conn = require("./connection.js");
 
-// function questMarks(num) {
-//   var arr = [];
-//   for (var i = 0; i < num; i++) {
-//     arr.push("?");
-//   }
-//   return arr.toString();
-// }
-
-// function objectToSql(ob) {
-//   var arr = [];
-//   for (var key in ob) {
-//     var value = ob[key];
-//     if (Object.hasOwnProperty.call(ob, key)) {
-//       if (typeof value === "string" && value.indexOf(" ") >= 0) {
-//         value = `"${value}"`;
-//       }
-//       arr.push(`${key}=${value}`);
-//     }
-//   }
-//   return arr.toString();
-// }
-
 var orm = {
   selectAll: function(input, cb) {
     var qS = "select * from " + input;
